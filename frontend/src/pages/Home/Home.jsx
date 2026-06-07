99af2da680bd01b8cf0446bf67b6353f6dacc21a
tamanna-2703 <tamannasachdeva27@gmail.com>
2026-06-07 23:24:14 +0530

    fix: resolve case-sensitivity issue in Home.css import

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, {useState}from 'react'
-import './home.css'
+import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
